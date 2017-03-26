ace24e2ad9bca3e4c710dda6d3dfa702a7f61df5
Jesús Ángel González Novez <jesusgonzaleznovez@gmail.com>
2017-03-26 13:03:37 +0200

    Saltamontes

--- game/escenas/main.gd
+++ game/escenas/main.gd
@@ -3,6 +3,7 @@ extends Node2D
 var player = load("res://escenas/Player.tscn")
 var babosa = load("res://escenas/EnemyEasy.tscn")
 var background = load("res://escenas/Background.tscn")
+var saltamontes = load("res://escenas/Saltamontes.tscn")
 var back = background.instance()
 var p = player.instance()
 
@@ -23,6 +24,10 @@ func _ready():
 
 
 func _on_Timer_timeout():
+	var s = saltamontes.instance()
+	self.add_child(s)
+	s.set_pos(Vector2(rand_range(0,global.RES_X),20))
+	
 	var b = babosa.instance()
 	self.add_child(b)
 	b.set_pos(Vector2(rand_range(0,global.RES_X),20))
